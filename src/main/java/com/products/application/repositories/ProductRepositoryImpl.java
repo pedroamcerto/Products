8bdc96c2977a1247730e167aa540cecdbed651ef
fabianoZagDev <fdopinho@gmail.com>
2025-05-13 22:45:16 -0300

    Product update repository

--- src/main/java/com/products/application/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/products/application/repositories/ProductRepositoryImpl.java
@@ -1,4 +1,19 @@
 package com.products.application.repositories;
 
-public class ProductRepository {
+import com.products.domain.Product;
+import com.products.ports.output.ProductRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductRepositoryImpl implements ProductRepository {
+    private final Map<String, Product> storage = new HashMap<>();
+
+    public void save(Product product){
+        storage.put(product.getId(),product);
+    }
+
+    public Product findById(String id){
+        return storage.get(id);
+    }   
 }
--- src/main/java/com/products/ports/output/ProductRepository.java
+++ src/main/java/com/products/ports/output/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.products.ports.output;
 
+import com.products.domain.Product;
+
 public interface ProductRepository {
+    void save(Product product);
+    Product findById(String id);
 }
