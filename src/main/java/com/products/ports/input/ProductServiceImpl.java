e576e1ec9755e85f20d96ad7d016b67766baad7d
Pedro Certo <113721862+pedroamcerto@users.noreply.github.com>
2025-05-13 22:49:14 -0300

    update: product service

--- src/main/java/com/products/ports/input/ProductServiceImpl.java
+++ src/main/java/com/products/ports/input/ProductServiceImpl.java
@@ -1,23 +1,28 @@
 package com.products.ports.input;
 
+import com.products.application.repositories.ProductRepositoryImpl;
 import com.products.application.usecases.ProductService;
 import com.products.domain.Product;
+import com.products.ports.output.ProductRepository;
 
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
+    private final ProductRepositoryImpl productRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    public ProductServiceImpl(ProductRepositoryImpl productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public void createProduct(String id,Product product) {
-        productRepository.save(id, product);
+        productRepository.save(product);
     }
 
     @Override
     public void updatePrice(String id, Double price) {
-        productRepository.updatePrice(id, price);
+        Product product = productRepository.findById(id);
+        product.setPrice(price);
+
+        productRepository.save(product);
     }
 }
